2d810b00ca7471813c97bd4c16d726b004a82412
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-12 09:20:11 +0530

    Quick Sort

--- 05Sorting/Comparison Based Sorting/quickSort.js
+++ 05Sorting/Comparison Based Sorting/quickSort.js
@@ -1,3 +1,18 @@
+// QUICK SORT
+// QuickSort is a sorting algorithm based on the Divide and Conquer algorithm.
+// It picks an element as a pivot and partitions the given array around the picked pivot by placing the pivot in its correct position in the sorted array.
+
+// Steps of Quick Sort
+// 1. Choose a Pivot:
+// Select an element from the array as the pivot. Various strategies can be used to choose the pivot (e.g., first element, last element, random element, or median).
+
+// 2. Partitioning:
+// Rearrange the array such that elements less than the pivot are on the left, elements greater than the pivot are on the right, and the pivot is in its final sorted position.
+// This step is crucial as it ensures that the pivot is in the correct place in the sorted array.
+
+// 3. Recursively Apply:
+// Recursively apply the above steps to the sub-arrays of elements with smaller values and elements with larger values.
+
 function quickSort(arr, start, end) {
   if (start < end) { // Base case: if subarray has 1 or 0 elements, it's already sorted
     let pivot = partition(arr, start, end);
