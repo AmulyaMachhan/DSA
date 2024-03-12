0b9faaf8fec95bca4816e0cbc776b16ea7c14379
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-13 02:34:42 +0530

    Pattern 24

--- 01Patterns/pattern24.js
+++ 01Patterns/pattern24.js
@@ -1,8 +1,10 @@
 function pattern(n){
     let str = "";
-    for(let i = n ; i > 0 ; i--){
-        for(let j = i ; j <= n ; j++){
-            str += j;
+    for(let i = 0 ; i < n ; i++){
+        let count = n - i;
+        for(let j = 0 ; j <= i ; j++){
+            str += count;
+            count++;
         }
         str += "\n"
     }
