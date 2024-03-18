636582b99d5dc6db48c31e5ec0ce510a3f5400dd
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-19 01:36:22 +0530

    DSA Everyday

--- arrays/medium/ElementThatAppearsNby2Times.js
+++ arrays/medium/ElementThatAppearsNby2Times.js
@@ -1,3 +1,34 @@
+//Optimal Approach
+//Moore Voting Algorithm
+function ElementThatAppearsNby2Times_1(array){
+    
+    let count = 0;
+    let el ;
+    for(let i = 0 ; i < array.length ; i++){
+        if(count == 0){
+            count = 1;
+            el = array[i];
+        }else if(el == array[i]){
+            count++;
+        }else{
+            count--;
+        }
+    } 
+
+    let count_1 = 0; 
+    for(let i = 0 ; i<array.length ; i++){
+        if(array[i] == el){
+            count_1++;
+        }
+    }
+    console.log(el);
+    return el;
+}
+console.log(ElementThatAppearsNby2Times_1([2,1,1,1,2]))
+console.log(ElementThatAppearsNby2Times_1([3,2,3]))
+console.log(ElementThatAppearsNby2Times_1([4,4,2,4,3,4,4,3,2,4]))
+
+//Better Approach
 function ElementThatAppearsNby2Times(array){
     let hashMap = {};
     for(let i = 0 ; i < array.length ; i++){
