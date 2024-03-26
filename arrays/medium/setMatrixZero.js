6fff6aebc9254585e7205bb20c0fd8ec932c49ef
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-26 17:29:00 +0530

    DSA Everyday

--- arrays/medium/setMatrixZero.js
+++ arrays/medium/setMatrixZero.js
@@ -22,3 +22,48 @@ console.table(setMatrixZero([
     [1, 0, 1],
     [1, 1, 1],
   ]))
+  
+  function setMatrixZero_1(array){
+      let rows = array.length; 
+      let cols = array[0].length;
+
+    const zeroRows = new Array(rows).fill(false);
+    const zeroCols = new Array(cols).fill(false);
+    
+    for(let  i = 0 ; i < rows ; i++){
+        for(let  j = 0 ; j < cols ; j++){
+            if(array[i][j] === 0){
+                zeroRows[i] = true;
+                zeroCols[j] = true;
+            }
+        }
+    }
+
+    for(let  i = 0 ; i < rows ; i++){
+        if(zeroRows[i]){
+            for(let  j = 0 ; j < cols ; j++){
+                array[i][j] = 0;
+            }            
+        }
+    }
+    for(let  j = 0 ; j < cols ; j++){
+        if(zeroCols[j]){
+            for(let  i = 0 ; i < rows ; i++){
+                array[i][j] = 0;
+            }
+        }
+    }
+    
+    return array;
+}
+console.table(setMatrixZero_1([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ]));
+
+console.table(setMatrixZero_1([
+    [0,1,2,0],
+    [3,4,5,2],
+    [1,3,1,5]
+]));
