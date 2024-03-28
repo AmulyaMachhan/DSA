4b7bb89ed222b142529886711ff3225d724508e0
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-28 15:07:50 +0530

    DSA Everday

--- arrays/medium/setMatrixZero.js
+++ arrays/medium/setMatrixZero.js
@@ -1,3 +1,13 @@
+//OPTIMAL APPROACH 
+//Here the space complexity is reduced to almost constant
+//Time complexity = o(n^2)
+//First, we will traverse the matrix and mark the proper cells of 1st row and 1st column with 0 accordingly. The marking will be like this: if cell(i, j) contains 0, we will mark the i-th row i.e. matrix[i][0] with 0 and we will mark j-th column i.e. matrix[0][j] with 0.
+//If i is 0, we will mark matrix[0][0] with 0 but if j is 0, we will mark the col0 variable with 0 instead of marking matrix[0][0] again.
+//After step 1 is completed, we will modify the cells from (1,1) to (n-1, m-1) using the values from the 1st row, 1st column, and col0 variable.
+//We will not modify the 1st row and 1st column of the matrix here as the modification of the rest of the matrix(i.e. From (1,1) to (n-1, m-1)) is dependent on that row and column.
+//Finally, we will change the 1st row and column using the values from matrix[0][0] and col0 variable. Here also we will change the row first and then the column.
+//If matrix[0][0] = 0, we will change all the elements from the cell (0,1) to (0, m-1), to 0.
+//If col0 = 0, we will change all the elements from the cell (0,0) to (n-1, 0), to 0.
 function setMatrixZero_2(matrix){
     let col0 = 1;
     let n = matrix.length;
