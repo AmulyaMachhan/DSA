53b68d44b5554b4dd2be0c8fbc4b0d30f9b3cadc
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-29 00:42:38 +0530

    DSA Everday

--- arrays/medium/rotateMatrixBy90.js
+++ arrays/medium/rotateMatrixBy90.js
@@ -1,3 +1,6 @@
+//OPTIMAL APPROACH
+//TIME COMPLEXITY = O(N^2)
+//SPACE COMPLEXITY = O(1)
 function rotateMatrixBy90(matrix){
     for(let i = 0; i < matrix.length ; i++ ){
         for(let j=i ; j < matrix[0].length ; j++){
@@ -17,6 +20,23 @@ function rotateMatrixBy90(matrix){
 }
 
 console.table(rotateMatrixBy90([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]));
+
+//BRUTE FORCE APPROACH 
+function rotateMatrixBy90_1(matrix){
+    const rotated = new Array(matrix.length).fill().map( ()=> Array(matrix[0].length).fill(0));
+
+    for(let i = 0 ; i < matrix.length ; i++){
+        for(let  j  = 0 ; j < matrix[0].length ; j++){
+            rotated[j][matrix.length - i - 1] = matrix[i][j];
+        }
+    }
+    return rotated;
+}
+console.table(rotateMatrixBy90_1([
     [1,2,3],
     [4,5,6],
     [7,8,9]
