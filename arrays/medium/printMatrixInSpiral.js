c921101382dbb06b9c57d4086d6ed43597bc2917
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-30 19:29:43 +0530

    DSA Everday

---
+++ arrays/medium/printMatrixInSpiral.js
@@ -0,0 +1,36 @@
+function printMatrixInSpiral(matrix) {
+    let top = 0, left = 0, right = matrix[0].length - 1, bottom = matrix.length - 1;
+
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            console.log(matrix[top][i]);
+        }
+        top++;
+
+        for (let i = top; i <= bottom; i++) {
+            console.log(matrix[i][right]);
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                console.log(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                console.log(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+}
+
+printMatrixInSpiral([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]);
