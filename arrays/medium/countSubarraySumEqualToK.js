3c5fe08704b4e85dd4b4b464161aa3772283e033
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-31 20:57:59 +0530

    DSA Everyday

--- arrays/medium/countSubarraySumEqualToK.js
+++ arrays/medium/countSubarraySumEqualToK.js
@@ -1,34 +1,33 @@
 //Optimal approach 
 //Using a hashMap
-function countSubarraySumEqualToK_4(array , target){
-    let count = 0 ;
-    let size = array.length - 1;
-    let hashMap = {};
-    let sum = 0;
+function countSubarraySumEqualToK_4(array , k){
+    let n = array.length; // size of the given array.
+    let mpp = new Map();
+    let preSum = 0, cnt = 0;
 
-    for(let i = 0 ; i <=size ; i++){
-        sum += array[i];
+    mpp.set(0, 1); // Setting 0 in the map.
+    for (let i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += array[i];
 
-        if(sum == target){
-            count++;
-        }
+        // Calculate x-k:
+        let remove = preSum - k;
 
-        let rem = target - sum;
-        if(hashMap[rem]){
-            count++;
-        }
+        // Add the number of subarrays to be removed:
+        cnt += mpp.get(remove) || 0;
 
-        if(!hashMap[sum]){
-            hashMap[sum] = i;
-        }
+        // Update the count of prefix sum
+        // in the map.
+        mpp.set(preSum, (mpp.get(preSum) || 0) + 1);
     }
-    return count;
+    console.table(mpp)
+    return cnt;
 }
 console.log(countSubarraySumEqualToK_4([3, 1, 2, 4], 6));
 console.log(countSubarraySumEqualToK_4([1], 1));
 console.log(countSubarraySumEqualToK_4([1], 0));
 console.log(countSubarraySumEqualToK_4([-1,1,-1], 0));
-console.log(countSubarraySumEqualToK_4([-1,-1,-1], -3));// Here this method fails because it cannot count sum for negative values
+console.log(countSubarraySumEqualToK_4([-1,-1,-1], -3));
 console.log("\n")
 
 //Only if all the elements in the array are positive 
