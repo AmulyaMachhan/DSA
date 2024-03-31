b3e79958b890073217db8bdd50ce43159d743a3e
AmulyaMachhan <machhanamulya@gmail.com>
2024-03-31 20:43:53 +0530

    DSA Everyday

--- arrays/medium/countSubarraySumEqualToK.js
+++ arrays/medium/countSubarraySumEqualToK.js
@@ -1,3 +1,36 @@
+//Optimal approach 
+//Using a hashMap
+function countSubarraySumEqualToK_4(array , target){
+    let count = 0 ;
+    let size = array.length - 1;
+    let hashMap = {};
+    let sum = 0;
+
+    for(let i = 0 ; i <=size ; i++){
+        sum += array[i];
+
+        if(sum == target){
+            count++;
+        }
+
+        let rem = target - sum;
+        if(hashMap[rem]){
+            count++;
+        }
+
+        if(!hashMap[sum]){
+            hashMap[sum] = i;
+        }
+    }
+    return count;
+}
+console.log(countSubarraySumEqualToK_4([3, 1, 2, 4], 6));
+console.log(countSubarraySumEqualToK_4([1], 1));
+console.log(countSubarraySumEqualToK_4([1], 0));
+console.log(countSubarraySumEqualToK_4([-1,1,-1], 0));
+console.log(countSubarraySumEqualToK_4([-1,-1,-1], -3));// Here this method fails because it cannot count sum for negative values
+console.log("\n")
+
 //Only if all the elements in the array are positive 
 function countSubarraySumEqualToK(array, target) {
     let size = array.length - 1;
