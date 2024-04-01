9c3cf1ca71979b8c625632eef27328e4a45bcd49
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-01 18:31:22 +0530

    DSA Everyday

--- arrays/hard/pascalsTriangle.js
+++ arrays/hard/pascalsTriangle.js
@@ -23,15 +23,17 @@ console.log(pascalsTriangle(5));
 
 // Variation-1
 // Given row number r and column number c, print the element at position (r,c) in pascal's triangle
-function pascalsTriangle(number , row , column){
-    const array = [];
-    for(let i = 1 ; i <= row ; i++){
-        array[i] = [];
-        for(let j = 1 ; j <= i; j++){
-            if(j==1 || j == i){
-                array[i][j] = 1;
-                
-            }
-        }
-    }
+function pascalsTriangle_1(row , column){
+    return nCr(row - 1 , column - 1);
 }
+
+function nCr(n , r){
+    let res = 1; 
+    for(let i = 0 ; i < r ; i++){
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+console.log(pascalsTriangle_1(5,3))
+console.log(pascalsTriangle_1(4,4))
