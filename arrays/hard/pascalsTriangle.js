2208048e64fe099d791f2171fd3bfd1ed4614118
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-01 18:49:51 +0530

    DSA Everyday

--- arrays/hard/pascalsTriangle.js
+++ arrays/hard/pascalsTriangle.js
@@ -23,6 +23,10 @@ console.log(pascalsTriangle(5));
 
 // Variation-1
 // Given row number r and column number c, print the element at position (r,c) in pascal's triangle
+// Here we use combination that is nCr which gives us the exact answer to return the exact element in pascal's triangle
+// Time Complexity: O(c), where c = given column number.
+// Reason: We are running a loop for r times, where r is c-1.
+// Space Complexity: O(1) as we are not using any extra space.
 function pascalsTriangle_1(row , column){
     return nCr(row - 1 , column - 1);
 }
@@ -37,3 +41,15 @@ function nCr(n , r){
 }
 console.log(pascalsTriangle_1(5,3))
 console.log(pascalsTriangle_1(4,4))
+
+//Variation 2
+// Print the whole row given for the pascal triangle 
+function pascalsTriangle_2(row){
+    let resArray = []
+    for(let i = 0 ; i <= row ; i++){
+        resArray.push(nCr(row , i));
+    }
+    return resArray;
+}
+
+console.log(pascalsTriangle_2(4));
