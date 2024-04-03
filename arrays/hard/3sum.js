c9f7b31de82d6a236e6675bb5f1fe0a6a56d2ea3
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-03 16:58:30 +0530

    DSA Everyday

--- arrays/hard/3sum.js
+++ arrays/hard/3sum.js
@@ -1,6 +1,37 @@
+//OPTIMAL APPROACH
+function threeSum_3(array){
+    let res = [];
+    let n = array.length;
+    array.sort((a,b) => a -b)
+    for(let i = 0 ; i < n ; i++){
+        if(i != 0 && array[i] == array[i -1]) continue;
+
+        let j = i + 1 ;
+        let k = n - 1;
+        while(j < k){
+            let sum = array[i] + array[j] + array[k];
+            if(sum < 0){
+                j++;
+            }else if(sum > 0){
+                k--;
+            }else{
+                let temp = [array[i] , array[j] , array[k]];
+                res.push(temp);
+                j++;
+                k--;
+
+                while(j < k && array[j] == array[j-1]) j++;
+                while(j < k && array[k] == array[k+1]) k--;
+            }
+        } 
+    }
+    return res;
+}
+console.table(threeSum_3([-1, 0, 1, 2, -1, -4]))
+console.table(threeSum_3([-1,0,1,0]))
 
 //BETTER APPROACH
-function ThreeSum_1(array){
+function threeSum_1(array){
     let n = array.length;
     let res = []
     for(let i = 0 ; i < n ; i++){
@@ -22,11 +53,11 @@ function ThreeSum_1(array){
     res = Array.from(set).map(JSON.parse);
     return res;
 }
-console.table(ThreeSum_1([-1, 0, 1, 2, -1, -4]))
-console.table(ThreeSum_1([-1,0,1,0]))
+console.table(threeSum_1([-1, 0, 1, 2, -1, -4]))
+console.table(threeSum_1([-1,0,1,0]))
 
 //BRUTE FORCE APPROACH
-function ThreeSum(array){
+function threeSum(array){
     let n = array.length;
     let ans = [];
     for(let i= 0 ; i < n ; i++){
@@ -45,4 +76,4 @@ function ThreeSum(array){
     return ans;
 }
 
-console.table(ThreeSum([-1, 0, 1, 2, -1, -4]))
+console.table(threeSum([-1, 0, 1, 2, -1, -4]))
