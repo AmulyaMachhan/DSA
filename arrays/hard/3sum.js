421d76c736e149071e4d9bc722bcee7aeb57404d
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-03 16:35:32 +0530

    DSA Everday

--- arrays/hard/3sum.js
+++ arrays/hard/3sum.js
@@ -1,3 +1,28 @@
+//BETTER APPROACH
+function ThreeSum_1(array){
+    let n = array.length;
+    let res = []
+    for(let i = 0 ; i < n ; i++){
+        let hashMap = {}
+        for(let j = i+1 ; j < n ; j++){
+            let third = -(array[i] + array[j]);
+
+            if(hashMap[third]){
+                let temp = [array[i] ,array[j], third];
+                temp.sort((a,b) => a - b);
+                res.push(temp);
+            }
+
+            hashMap[array[j]] = j;
+        }
+    }
+    
+    let set  = new Set(res.map(JSON.stringify));
+    res = Array.from(set).map(JSON.parse);
+    return res;
+}
+console.table(ThreeSum_1([-1, 0, 1, 2, -1, -4]))
+
 //BRUTE FORCE APPROACH
 function ThreeSum(array){
     let n = array.length;
