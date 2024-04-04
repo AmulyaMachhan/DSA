b72eb71485347bdabd550b04dc67be1ec06499f5
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-04 12:25:27 +0530

    DSA Everyday

--- arrays/hard/longestSubarrayWith0Sum.js
+++ arrays/hard/longestSubarrayWith0Sum.js
@@ -1,3 +1,4 @@
+//OPTIMAL APPROACH
 function longestSubarrayWith0Sum(array){
     let hashMap = new Map();
     let maxLen = 0
@@ -21,3 +22,20 @@ function longestSubarrayWith0Sum(array){
 
 console.log(longestSubarrayWith0Sum([9, -3, 3, -1, 6, -5]));
 console.log(longestSubarrayWith0Sum([6, -2, 2, -8, 1, 7, 4, -10]));
+
+//BRUTE FORCE APPROACH
+function longestSubarrayWith0Sum_1(array){
+    let maxLen = 0 ;
+    for(let i = 0 ; i < array.length ; i++){
+        let sum = 0;
+        for(let j = i; j < array.length ; j++){
+            sum += array[j];
+            if(sum == 0){
+                maxLen = Math.max(maxLen , j - i + 1);
+            }
+        }
+    }
+    return maxLen;
+}
+console.log(longestSubarrayWith0Sum_1([9, -3, 3, -1, 6, -5]));
+console.log(longestSubarrayWith0Sum_1([6, -2, 2, -8, 1, 7, 4, -10]));
