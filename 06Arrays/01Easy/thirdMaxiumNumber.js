4c6e72d64f0e07f8e5da4a2d3282da7e5ec47e80
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-10 12:55:44 +0530

    Third Maximum Number

--- 06Arrays/01Easy/thirdMaxiumNumber.js
+++ 06Arrays/01Easy/thirdMaxiumNumber.js
@@ -30,12 +30,37 @@
 // 1 <= nums.length <= 104
 // -231 <= nums[i] <= 231 - 1
 
+// OPTIMAL APPRACH
+function thirdMaximumNumber_1(array){
+    let max = -Infinity;
+    let secondMax= -Infinity;
+    let thirdMax= -Infinity;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] > max){
+            thirdMax = secondMax;
+            secondMax = max;
+            max = array[i];
+        }else if(max > array[i] && array[i] > secondMax){
+            thirdMax = secondMax; 
+            secondMax = array[i];
+        }else if(secondMax > array[i] && array[i] > thirdMax){
+            thirdMax = array[i];
+        }
+    }
+    return thirdMax == -Infinity ? max : thirdMax;
+}
+
+console.log(thirdMaximumNumber_1([3,2,1]));
+console.log(thirdMaximumNumber_1([2,1]));
+console.log(thirdMaximumNumber_1([2,2,3,1]));
+
 // BETTER APPROACH
 function thirdMaximumNumber(array){
+    array = [...new Set(array)];
     if(array.length === 1) return array[0];
     if(array.length === 2) return Math.max(array[0], array[1]);
 
-    array.sort((a,b) => a - b);
+    array.sort((a,b) => b - a);
     return array[2];
 }
 
