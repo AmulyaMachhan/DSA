4a4e7244ab0fb8f2caafbc04dd3acadf4dc3053c
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-13 23:59:21 +0530

    DSA Everyday

--- arrays/hard/repeatingAndMissingNumbers.js
+++ arrays/hard/repeatingAndMissingNumbers.js
@@ -1,3 +1,28 @@
+//OPTIMAL APPROACH
+function repeatingAndMissingNumbers_1(array){
+    let n = array.length;
+
+    let SN = (n*(n+1))/2;
+    let S2N = (n*(n+1)*(2*n-1))/6;
+
+    let S=0 , S2=0;
+    for(let i = 0 ; i < n ; i++){
+        S += array[i];
+        S2 += array[i] * array[i];
+    }
+
+    let val1 = S - SN;
+    let val2 = (S2 - S2N)/val1;
+
+    let X = (val1 + val2)/2;
+    let Y = X - val1;
+
+    return [X , Y];
+}
+
+console.log(repeatingAndMissingNumbers_1([3,1,2,4,2]))
+
+//BETTER APPROACH
 function repeatingAndMissingNumbers(array){
     let hashMap = {};
 
