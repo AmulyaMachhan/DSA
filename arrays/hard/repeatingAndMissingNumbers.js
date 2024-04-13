f1add5891779e050ce1e9da291a0c1a8e712d247
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-13 21:53:08 +0530

    DSA Everyday

--- arrays/hard/repeatingAndMissingNumbers.js
+++ arrays/hard/repeatingAndMissingNumbers.js
@@ -2,18 +2,25 @@ function repeatingAndMissingNumbers(array){
     let hashMap = {};
 
     for(let i = 0; i < array.length ; i++){
-        if(hashMap(array[i])){
-            hashMap(array[i])++;
+        if(hashMap[array[i]]){
+            hashMap[array[i]]++;
         }else{
-           hashMap(array[i]) = 1;
+           hashMap[array[i]] = 1;
         }
     }
-    
-    for(let key in hashMap){
-        if(hashMap[key] > 1){
-            return key;
+    let repeating;
+    let missing;
+    for(let i = 0; i < array.length ; i++){
+        if(hashMap[array[i]] == 2){
+            repeating = array[i];
+        }
+
+        if(hashMap[array[i]]){
+            missing = array[i]
         }
     }
+
+    return repeating , missing;
 }
 
 console.log(repeatingAndMissingNumbers([3,1,2,5,3]))
