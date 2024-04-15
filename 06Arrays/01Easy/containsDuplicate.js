4f44e56fd40ac4f82a15be7c94185775b46011a7
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-16 00:01:01 +0530

    Contains Duplicate

---
+++ 06Arrays/01Easy/containsDuplicate.js
@@ -0,0 +1,43 @@
+// PROBLEM STATEMENT 
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// OPTIMAL APPROACH
+// Using set data structure
+function containsDuplicate_2(array){
+    return array.length !== new Set(array).size 
+}
+
+console.log(containsDuplicate_2([1,1,1,2,3]));
+console.log(containsDuplicate_2([1,2,3]));
+
+// APPROACH 1 
+// Using hashMap data structure
+function containsDuplicate(array){
+    let map = new Map();
+
+    for(let items of array){
+        if(map.has(items)) return true;
+        else map.set( items , 1)
+    }
+
+    return false;
+}
+
+console.log(containsDuplicate([1,1,1,2,3]));
+console.log(containsDuplicate([1,2,3]));
+
+// APPROACH 2
+// USING SORTING;
+function containsDuplicate_1(array){
+    array.sort((a , b) => a- b);
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === array[i+1]){
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(containsDuplicate_1([1,1,1,2,3]));
+console.log(containsDuplicate_1([1,2,3]));
