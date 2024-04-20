2036b5f528452c40ddf78fb25e591ef7331f4c9e
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-20 23:31:49 +0530

    DSA Everyday

--- arrays/hard/reversePairs.js
+++ arrays/hard/reversePairs.js
@@ -1,3 +1,56 @@
+//OPTIMAL APPROACH
+function countInversions_1(array){
+    return mergeSort(array , 0 , array.length - 1)
+}
+function mergeSort(array , low , high){
+    let count = 0;
+
+    if(low  >= high ){
+        return count
+    }
+    let mid = Math.floor((low  + high)/2);
+
+    count += mergeSort(array , 0 , mid);
+    count += mergeSort(array , mid + 1 , high);
+    count += merge(array , low , mid , high);
+
+    return count;
+}
+
+function merge(array , low , mid , high){
+    let leftIndex = low
+    let rightIndex = mid + 1;
+    let result = [];
+    let count = 0;
+    while(leftIndex <= mid && rightIndex <= high){
+        if(array[leftIndex] <= array[rightIndex]){
+            result.push(array[leftIndex]);
+            leftIndex++;
+        }
+        else{
+            result.push(array[rightIndex]);
+            count += (mid - leftIndex + 1);
+            rightIndex++;
+        }
+    }
+
+    while(leftIndex <= mid){
+        result.push(array[leftIndex])
+        leftIndex++;
+    }
+    while(rightIndex <= high){
+        result.push(array[rightIndex])
+        rightIndex++;
+    }
+
+    for(let i = low ; i <= high ; i++){
+        array[i] = result[i - low]
+    }
+
+    return count;
+}
+
+//BRUTE FORCE METHOD
 function reversePairs(array){
     let count = 0 ;
     for(let i = 0 ; i < array.length ; i++){
