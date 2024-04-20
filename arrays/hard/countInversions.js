52d01fce6bb9b68018711c4e4c4e59212239dc05
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-20 22:29:25 +0530

    DSA Everyday

--- arrays/hard/countInversions.js
+++ arrays/hard/countInversions.js
@@ -2,7 +2,7 @@
 function countInversions(array){
     let count = 0;
     for(let i = 0 ; i < array.length ; i++){
-        for(let j = i ; j < array.length ; j++){
+        for(let j = i + 1 ; j < array.length ; j++){
             if(array[i] > array[j]){
                 count++;
             }
