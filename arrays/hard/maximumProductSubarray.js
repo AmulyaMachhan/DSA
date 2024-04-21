75ed73daf46863f8e96f242849b1a0cf0bf993de
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-21 22:07:55 +0530

    DSA Everyday

--- arrays/hard/maximumProductSubarray.js
+++ arrays/hard/maximumProductSubarray.js
@@ -21,6 +21,28 @@ console.log(maximumProductSubarray([1,2,3,4,5,0]));
 console.log(maximumProductSubarray([1,2,-3,0,-4,-5]));
 console.log(maximumProductSubarray([1,1,0, -3, 0 ,45, 0 ,23]));
 
+//OPTIMAL APPROACH - 2 
+//Kadane's algorithm modification
+function maximumProductSubarray_1(array){
+    let prod1 = array[0];
+    let prod2 = array[0];
+    let result = array[0];
+    
+    for(let i = 1 ; i< array.length ;i++){
+        prod1 = Math.max(array[i] , array[i] * prod1 , array[i]*prod2);
+        prod2 = Math.min(array[i] , array[i] * prod1 , array[i]*prod2);
+        
+        result = Math.max(result , prod1);
+    }
+    
+    return result;
+}
+
+console.log(maximumProductSubarray_1([1,2,3,4,5,0]));
+console.log(maximumProductSubarray_1([1,2,-3,0,-4,-5]));
+console.log(maximumProductSubarray_1([1,1,0, -3, 0 ,45, 0 ,23]));
+
+
 //BRUTE FORCE APPROACH
 function maximumProductSubarry(array){
     let maxProd = array[0];
