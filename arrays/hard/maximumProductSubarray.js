5f983a8fb4db79896a70f42e51ef06537054e733
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-21 21:55:37 +0530

    DSA Everyday

--- arrays/hard/maximumProductSubarray.js
+++ arrays/hard/maximumProductSubarray.js
@@ -1,13 +1,37 @@
+//OPTIMAL APPROACH -1
+//observational approach
+function maximumProductSubarray(array){
+    let suffix = 1, prefix = 1;
+    let max= -Infinity;
+    let n = array.length
+    for(let i = 0 ; i < array.length ; i++){
+        if(prefix === 0) prefix = 1;
+        if(suffix === 0) suffix = 1;
+
+        prefix *= array[i];
+        suffix *= array[n - i - 1];
+
+        max = Math.max(max , Math.max(prefix , suffix));
+    }
+
+    return max;
+}
+
+console.log(maximumProductSubarray([1,2,3,4,5,0]));
+console.log(maximumProductSubarray([1,2,-3,0,-4,-5]));
+console.log(maximumProductSubarray([1,1,0, -3, 0 ,45, 0 ,23]));
+
 //BRUTE FORCE APPROACH
 function maximumProductSubarry(array){
-    let maxProd = 1;
-    for(let i = 0 ; i < array.length ; i++){
+    let maxProd = array[0];
+    for(let i = 0 ; i < array.length - 1 ; i++){
         let prod = 1;
         for(let j = i ; j < array.length ;j++){
-            prod *= array[j]
-            
             maxProd = Math.max(prod , maxProd)
+            
+            prod *= array[j]
         }
+        maxProd = Math.max(prod , maxProd)
     }
 
     return maxProd;
