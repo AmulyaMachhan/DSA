19853cf1db2b96937b7114908eb4b0c17325ed91
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-21 22:56:20 +0530

    DSA Everyday

--- 07Binary search/01 BS On Arrays/simpleBinarySearch.js
+++ 07Binary search/01 BS On Arrays/simpleBinarySearch.js
@@ -15,5 +15,17 @@ function simpleBinarySearch(array , target){
     return -1;
 }
 
-console.log(simpleBinarySearch([3, 4, 6, 7, 9, 12, 16, 17], 6))
-console.log(simpleBinarySearch([3, 4, 6, 7, 9, 12, 16, 17], 0))
+console.log(simpleBinarySearch([3, 4, 6, 7, 9, 12, 16, 17], 6));
+console.log(simpleBinarySearch([3, 4, 6, 7, 9, 12, 16, 17], 0));
+
+//RECURSIVE APPROACH
+function simpleBinarySearch_1(array , target , low , high){
+    if(low > high) return -1;
+    
+    let mid = Math.floor((low + high)/2);
+    if(array[mid] === target) return mid + 1;
+    else if(array[mid] > target) return simpleBinarySearch_1(array , target , low , mid - 1);
+    return simpleBinarySearch_1(array , target , mid + 1 , high);
+}
+console.log(simpleBinarySearch_1([3, 4, 6, 7, 9, 12, 16, 17], 6 , 0 , 7));
+console.log(simpleBinarySearch_1([3, 4, 6, 7, 9, 12, 16, 17], 0 , 0 , 7));
