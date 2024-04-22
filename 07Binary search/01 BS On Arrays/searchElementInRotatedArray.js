c2186009075b612a3ba9f69d420a570df28e8cc6
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-22 22:25:57 +0530

    DSA Everday

---
+++ 07Binary search/01 BS On Arrays/searchElementInRotatedArray.js
@@ -0,0 +1,27 @@
+function searchElementInRotatedArray(array , target){
+    let low = 0 , high = array.length;
+    
+    while(low <= high){
+        let mid = low + Math.floor((high - low)/ 2);
+        
+        if(array[mid] === target){
+            return mid;
+        }else if(array[mid] > target){
+            if(array[low] <= target && target >= array[mid]){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }else{
+            if(array[mid] <= target && target >= array[high]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+console.log(searchElementInRotatedArray([4,5,6,7,0,1,2,3] , 0));
+console.log(searchElementInRotatedArray([4,5,6,7,0,1,2] , 3));
