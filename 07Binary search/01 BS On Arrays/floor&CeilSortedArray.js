3eeb92b60412b70d89a7e6b5762d902c2434f15d
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-22 18:36:03 +0530

    DSA Everyday

--- 07Binary search/01 BS On Arrays/floor&CeilSortedArray.js
+++ 07Binary search/01 BS On Arrays/floor&CeilSortedArray.js
@@ -1,3 +1,28 @@
+//BETTER APPRAOCH
+function floorAndCeilInSortedArray_1(array , target){
+    let n = array.length;
+    let low = 0, high = n - 1;
+    let floor = -1; 
+    let ceil = Infinity;
+
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2);
+        
+        if(array[mid] === target){
+            return[target , target];
+        }else if(array[mid] < target){
+            floor = array[mid];
+            low = mid + 1;
+        }else{
+            ceil = array[mid];
+            high = mid - 1;
+        }
+    }
+    return [floor , ceil];
+}
+
+console.log(floorAndCeilInSortedArray_1([3, 4, 4, 7, 8, 10] , 5))
+
 function floorAndCeilInSortedArray(array , target){
     return [floor(array ,target) , ceil(array ,target)];
 }
