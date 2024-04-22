252f6b6e57d0093b488f772a5f7cc91dd9f1b0fe
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-22 23:49:06 +0530

    DSA Everyday

--- 07Binary search/01 BS On Arrays/searchElementInRotatedArrayWithDuplicateValues.js
+++ 07Binary search/01 BS On Arrays/searchElementInRotatedArrayWithDuplicateValues.js
@@ -8,6 +8,11 @@ function searchElementInRotatedArrayWithDuplicateValues(array ,target){
             return true;
         }
         
+        if(array[low] == array[mid] && array[mid] == array[high]){
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
         if(array[low] <= array[mid]){
             if(array[low] >= target && target < array[mid]){
                 high = mid - 1;
@@ -30,4 +35,7 @@ console.log(searchElementInRotatedArrayWithDuplicateValues(
 ));
 console.log(searchElementInRotatedArrayWithDuplicateValues(
     [7, 8, 1, 2, 3, 3, 3, 4, 5, 6] , 10
+));
+console.log(searchElementInRotatedArrayWithDuplicateValues(
+    [4, 1, 2, 3, 3, 3, 4] , 4
 ));
