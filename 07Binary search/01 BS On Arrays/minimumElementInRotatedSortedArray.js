8527605e3576f5053d5d16638bd03f136bcf6939
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-23 17:24:26 +0530

    DSA Everyday

--- 07Binary search/01 BS On Arrays/minimumElementInRotatedSortedArray.js
+++ 07Binary search/01 BS On Arrays/minimumElementInRotatedSortedArray.js
@@ -1,3 +1,36 @@
+//OPTIMAL APPROACH (Further optimisation to binary search)
+//Time Complexity = O(logN) => N = array size
+function minimumElementInRotatedSortedArray_1(array){
+    let low = 0 , high = array.length - 1;
+    let min = Infinity;
+
+    while(low <= high){
+        let mid = low + Math.floor((high - low)/2);
+
+        //if the entire search space between the low and high indexes is sorted
+        //this means the min is at the low pointer
+        if(array[low] <= array[high]){
+            min = Math.min(min , array[low]);
+            break;
+        }
+
+        //if left half is sorted
+        if(array[low] <= array[mid]){
+            min = Math.min(min , array[low]);
+
+            //eliminate the left half
+            low = mid + 1;
+        }
+        //if right half is sorted
+        else{
+            min = Math.min(min , array[mid]);
+            
+            //eliminate the right half
+            high = mid - 1;
+        }
+    }
+}
+
 //BETTER APPROACH
 function minimumElementInRotatedSortedArray(array){
     let low = 0, high = array.length - 1;
@@ -26,3 +59,7 @@ function minimumElementInRotatedSortedArray(array){
 
 console.log(minimumElementInRotatedSortedArray([4,5,6,7,0,1,2,3]));
 console.log(minimumElementInRotatedSortedArray([3,4,5,1,2]));
+
+//BRUTE FORCE APPROACH
+//Using the linear search we can directly find the lowest element 
+//This will give the time complexity O(n)
