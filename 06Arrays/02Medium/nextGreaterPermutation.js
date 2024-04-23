b99b31ceb6665c9e14fa31a36f792f053082e250
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-23 22:48:08 +0530

    DSA Everyday

--- 06Arrays/02Medium/nextGreaterPermutation.js
+++ 06Arrays/02Medium/nextGreaterPermutation.js
@@ -1,3 +1,13 @@
+//Problem Statement: 
+//Given an array Arr[] of integers, rearrange the numbers of the given array into the lexicographically next greater permutation of numbers.
+
+//If such an arrangement is not possible, it must rearrange to the lowest possible order (i.e., sorted in ascending order).
+
+//Time Complexity: O(3N), where N = size of the given array
+//Finding the break-point, finding the next greater element, and reversal at the end takes O(N) for each, where N is the number of elements in the input array. This sums up to 3*O(N) which is approximately O(3N).
+
+//Space Complexity: Since no extra storage is required. Thus, its space complexity is O(1).
+
 function nextGreaterPermutation(array){
     let index = -1;
     let size = array.length - 1;
