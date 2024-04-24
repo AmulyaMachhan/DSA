e1457c25cfa5558edc676cc1a98a4e3786c569e2
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-24 23:12:32 +0530

    DSA Everyday

--- 07Binary search/02 BS On Answers/nthRootOfNumber.js
+++ 07Binary search/02 BS On Answers/nthRootOfNumber.js
@@ -1,19 +1,22 @@
-function nthRootOfNumber(number , root){
-    let low = 0 , high = number;
-    let ans = 0;
-    while(low <= high){
-        let mid = low + Math.floor((high - low)/2);
-
-        let val = Math.pow(mid , root);
-        if(val <= number){
-            low = mid + 1; 
-        }else{
-            high = mid - 1;
-        }
-    }
-
-    return high;
+function toFindWhereTheNumberExists(number, mid, power) {
+    let ans = Math.pow(mid, power);
+    if (ans > number) return 2;
+    if (ans === number) return 1;
+    return 0;
 }
 
-console.log(nthRootOfNumber(27 , 3));
-console.log(nthRootOfNumber(69 , 4));
+function nthRootOfNumber(number, root) {
+    let low = 1, high = number;
+    let val = 0;
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+        val = toFindWhereTheNumberExists(number, mid, root);
+        if (val === 1) return mid;
+        else if (val === 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+console.log(nthRootOfNumber(27, 3)); 
+console.log(nthRootOfNumber(69, 4))
