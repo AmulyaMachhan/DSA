d78c1f8fe31554554598a5bf05ba56ff793736e2
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-25 16:16:30 +0530

    DSA Everyday

--- 07Binary search/02 BS On Answers/SmallestDivisorGivenThreshold.js
+++ 07Binary search/02 BS On Answers/SmallestDivisorGivenThreshold.js
@@ -1,3 +1,35 @@
+//Problem Statement: 
+//You are given an array of integers 'arr' and an integer i.e. a threshold value 'limit'. 
+//Your task is to find the smallest positive integer divisor,
+//such that upon dividing all the elements of the given array by it, 
+//the sum of the division's result is less than or equal to the given threshold value.
+
+//OPTIMAL APPROACH
+function smallestDivisorGivenThreshold_1(array , threshold){
+    let n = array.length;
+    let low =0 ;
+    let high = Math.max.apply(null , array);
+    
+    while(low <= high){
+        let mid = low + Math.floor((high - low) / 2);
+        let sum = 0;
+        for(let i = 0 ; i < n ; i++){
+            sum += Math.ceil(array[i] / mid);
+        }
+        if(sum <= threshold){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+console.log(smallestDivisorGivenThreshold_1([2,4,6,8,10] , 15));
+console.log(smallestDivisorGivenThreshold_1([2,4,6,8,10] , 10));
+console.log(smallestDivisorGivenThreshold_1([1,2,3,4,5] ,8));
+
+//BRUTE FORCE APPROACH
 function smallestDivisorGivenThreshold(array , threshold){
     let max = Math.max.apply(null ,array);
 
