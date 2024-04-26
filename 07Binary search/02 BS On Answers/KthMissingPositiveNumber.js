834571801a8ec57a3b737dd0e2e88edac276aa9d
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-26 17:51:36 +0530

    DSA Everyday

--- 07Binary search/02 BS On Answers/KthMissingPositiveNumber.js
+++ 07Binary search/02 BS On Answers/KthMissingPositiveNumber.js
@@ -1,3 +1,23 @@
+//OPTIMAL APPROACH
+function KthMissingPositiveNumber_2(vector , k){
+    let low = 0 ; high = vector.length - 1;
+
+    while(low <= high){
+        let mid = low + Math.floor((high - low)/2);
+
+        let missing = vector[mid] - (mid + 1);
+        if(missing < k ){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return k + high + 1;
+}
+
+console.log(KthMissingPositiveNumber_2([4,7,9,10] , 1));
+console.log(KthMissingPositiveNumber_2([4,7,9,10] , 4));
+
 //BETTER APPROACH
 function KthMissingPositiveNumber_1(vector , k){
     for(let i = 0 ; i < vector.length ; i++){
