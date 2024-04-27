5c27f47d8b6a9b1d767b3bf7a5d5144a3c8ee455
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-27 18:47:25 +0530

    DSA Everyday

--- 07Binary search/02 BS On Answers/splitArray-LargestSum.js
+++ 07Binary search/02 BS On Answers/splitArray-LargestSum.js
@@ -1,3 +1,35 @@
+//PROBLEM STATEMENT
+//Given an integer array ‘A’ of size ‘N’ and an integer ‘K'. 
+//Split the array ‘A’ into ‘K’ non-empty subarrays such that the largest sum of any subarray is minimized. 
+//Your task is to return the minimized largest sum of the split.
+//A subarray is a contiguous part of the array.
+
+//OPTIMAL APPROACH
+function splitArrayLargestSum_1(array , noOfSubarrays){
+    let n = array.length;
+    if(n < noOfSubarrays) return -1;
+
+    array.sort((a,b) => a - b);
+
+    let low = Math.max(...array);
+    let high = array.reduce((acc , elem) => acc + elem , 0);
+
+    while(low <= high){
+        let mid = low + Math.floor((high - low) / 2);
+
+        if(noOfSubarraysForGivenSum(array , n , mid) >  noOfSubarrays){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        } 
+    }
+    return low;
+}
+
+console.log(splitArrayLargestSum_1([1,2,3,4,5] , 3));
+console.log(splitArrayLargestSum_1([1,3,5] , 3));
+
+//BRUTE FORCE APPROACH
 function splitArrayLargestSum(array , noOfSubarrays){
     let n = array.length;
     if(array.length < noOfSubarrays) return -1;
@@ -29,3 +61,8 @@ function noOfSubarraysForGivenSum(array , n , sum ){
 
 console.log(splitArrayLargestSum([1,2,3,4,5] , 3));
 console.log(splitArrayLargestSum([1,3,5] , 3));
+
+//Time Complexity: O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.
+//Reason: We are using a loop from max(arr[]) to sum(arr[]) to check all possible values of time. 
+//Inside the loop, we are calling the noOfSubarraysForGivenSum() function for each number. 
+//Now, inside the noOfSubarraysForGivenSum() function, we are using a loop that runs for N times.
