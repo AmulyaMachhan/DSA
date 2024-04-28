b621790a99c2be0fdf4540275810b97d3897c6e6
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-28 21:55:05 +0530

    DSA Everyday

--- 07Binary search/02 BS On Answers/medianTwoSortedArray.js
+++ 07Binary search/02 BS On Answers/medianTwoSortedArray.js
@@ -1,3 +1,8 @@
+//Problem Statement: 
+//Given two sorted arrays arr1 and arr2 of size m and n respectively , return the median of the two sorted arrays. 
+//The median is defined as the middle value of a sorted list of numbers. 
+//In case the length of the list is even, the median is the average of the two middle elements.
+
 //BRUTE FORCE APPROACH
 function medianTwoSortedArray( array1 , array2){
     let n1 = array1.length ;
@@ -23,3 +28,9 @@ function medianTwoSortedArray( array1 , array2){
 
 console.log(medianTwoSortedArray([1, 3, 5] , [2, 4, 6]));
 console.log(medianTwoSortedArray([1, 4, 7, 10, 12] , [2, 3, 6, 15]));
+
+//Time Complexity: O(n1+n2), where  n1 and n2 are the sizes of the given arrays.
+//Reason: We traverse through both arrays linearly.
+
+//Space Complexity: O(n1+n2), where  n1 and n2 are the sizes of the given arrays.
+//Reason: We are using an extra array of size (n1+n2) to solve this problem.
