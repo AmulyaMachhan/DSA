90e3095b707d96e6f14c426879789a8f31ab2e85
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-29 20:28:32 +0530

    DSA Everyday

--- 07Binary search/03 BS On 2DArrays/rowWithMaximumNoOfOnes.js
+++ 07Binary search/03 BS On 2DArrays/rowWithMaximumNoOfOnes.js
@@ -1,3 +1,43 @@
+//OPTIMAL APPROACH
+function rowWithMaximumNoOfOnes_1(matrix){
+    let r = matrix.length;
+    let c = matrix[0].length;
+    let maxCount = 0;
+    let index = -1;
+
+    for(let i = 0 ; i < r ; i++){
+        let cnt = c - lowerBound(matrix[i] , c , 1);
+        if(cnt > maxCount){
+            maxCount = cnt;
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+function lowerBound( array , size , k){
+    let low = 0;
+    let high = size - 1;
+    let ans = 0;
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2);
+
+        if(array[mid] >= k) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return  `Row with max ones is ${index + 1}`;
+}
+
+console.log(rowWithMaximumNoOfOnes_1([
+    [1,1,1],
+    [0,0,1],
+    [0,0,0]
+]))
+
 //BRUTE FORCE APPROACH
 function rowWithMaximumNoOfOnes(matrix){
     let r = matrix.length;
