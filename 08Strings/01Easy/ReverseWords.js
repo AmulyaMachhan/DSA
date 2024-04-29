faa5e0df8de0c5738260426abbe69b4e7e203bb0
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-29 22:41:42 +0530

    DSA Everyday

--- 08Strings/01Easy/ReverseWords.js
+++ 08Strings/01Easy/ReverseWords.js
@@ -1,3 +1,54 @@
+//OPTIMAL APPROACH
+function reverseWords_1(string) {
+    let reversed = '';
+    let word = '';
+    let i = 0;
+
+    // Iterate through each character in the string
+    while (i < string.length) {
+        // Skip leading spaces
+        while (string[i] === ' ' && i < string.length) {
+            i++;
+        }
+
+        // Construct each word
+        while (string[i] !== ' ' && i < string.length) {
+            word += string[i];
+            i++;
+        }
+
+        // Add the constructed word to the beginning of the result string
+        if (word !== '') {
+            if (reversed === '') {
+                reversed = word;
+            } else {
+                reversed = word + ' ' + reversed;
+            }
+            word = '';
+        }
+    }
+
+    return reversed;
+}
+
+console.log(reverseWords_1(["Vishrut loves Gym"]));
+
+//Time complexity: 𝑂(𝑛)
+// O(n), where n is the length of the input string. This is because the algorithm iterates through each character of the string once to construct the reversed string.
+//Space complexity: O(n). 
+//The space complexity is also linear with respect to the input string length n because the algorithm constructs a new string to hold the reversed words
+
+//BETTER APRROACH
+function reverseWords(s) {
+    // Split the string into an array of words
+    const words = s.trim().split(/\s+/);
+
+    // Reverse the order of the array
+    words.reverse();
+
+    // Join the words back into a string
+    return words.join(' ');
+}
 //BRUTE FORCE APPROACH
 function reverseWords(string){
     let str = "";
