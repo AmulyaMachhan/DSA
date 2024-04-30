e3b95d9af1aae1ac4caf766f849b36ae993355a5
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-30 11:00:00 +0530

    DSA Everyday

--- 08Strings/01Easy/rotateStrings.js
+++ 08Strings/01Easy/rotateStrings.js
@@ -5,6 +5,7 @@
 
 // For example, if s = "abcde", then it will be "bcdea" after one shift.
 
+// BRUTE FORCE APPROACH
 function rotateStrings(s1 , s2){
     
     for(let i = 0 ; i < s1.length ; i++){
@@ -22,3 +23,11 @@ function rotateStrings(s1 , s2){
 }
 
 console.log(rotateStrings("abcde" , "bcdea"));
+
+// Time Complexity: O(n×m) n is the length of s1 and m is the length of s2.
+// The function has a nested loop structure, where the outer loop iterates over the length of s1, and the inner loop iterates over the length of s2. 
+// Therefore, the time complexity is proportional to the product of the lengths of s1 and s2.
+
+// Space Complexity: O(m)
+// We are creating a new string s with a length equal to the length of s2 in each iteration of the outer loop. 
+// Therefore, the space complexity is proportional to the length of s2.
