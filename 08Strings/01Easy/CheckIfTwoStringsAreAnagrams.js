bf54c3d0a456857f87f74f3048d5724471296f4d
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-30 13:10:47 +0530

    DSA Everyday

--- 08Strings/01Easy/CheckIfTwoStringsAreAnagrams.js
+++ 08Strings/01Easy/CheckIfTwoStringsAreAnagrams.js
@@ -1,7 +1,9 @@
 //OPTIMAL APPROACH
 function checkIfTwoStringsAreAnagrams_1(str1 , str2){
     let freq = {};
+    //Both strings are of different lengths cannot be anagrams so return false;
 
+    if(str1.length !== str2.length) return false;
     for(let i = 0 ; i < str1.length ; i++){
         if(!freq[str1[i]]){
             freq[str1[i]] = 1;
@@ -28,6 +30,12 @@ function checkIfTwoStringsAreAnagrams_1(str1 , str2){
 
 console.log(checkIfTwoStringsAreAnagrams_1("cat" , "tac"));
 console.log(checkIfTwoStringsAreAnagrams_1("cat" , "tacee"));
+
+// Time Complexity: O(n), where n is the length of the longer string among str1 and str2. 
+// Both for loops iterate over the characters of the strings once, and the final loop iterates over the frequency object, which contains at most n entries.
+
+// Space Complexity: O(n), where n is the length of the longer string among str1 and str2. 
+// The space complexity is determined by the size of the frequency object freq, which can contain at most n unique characters. 
 
 //NAIVE APPROACH
 function checkIfTwoStringsAreAnagrams(str1  , str2){
