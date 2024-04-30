fe99489419131ff208ff977c075254b7df49b282
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-30 13:03:30 +0530

    DSA Everyday

--- 08Strings/01Easy/CheckIfTwoStringsAreAnagrams.js
+++ 08Strings/01Easy/CheckIfTwoStringsAreAnagrams.js
@@ -1,3 +1,34 @@
+//OPTIMAL APPROACH
+function checkIfTwoStringsAreAnagrams_1(str1 , str2){
+    let freq = {};
+
+    for(let i = 0 ; i < str1.length ; i++){
+        if(!freq[str1[i]]){
+            freq[str1[i]] = 1;
+        }else{
+            freq[str1[i]]++;        
+        }
+    }
+    
+    for(let i = 0 ; i < str2.length ; i++){
+        if(freq[str2[i]]){
+            freq[str2[i]]--;
+        }else{
+            freq[str2[i]] = -1;
+        }
+    }
+    console.log(freq)
+    for(let keys in freq){
+        if(freq[keys] !== 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(checkIfTwoStringsAreAnagrams_1("cat" , "tac"));
+console.log(checkIfTwoStringsAreAnagrams_1("cat" , "tacee"));
+
 //NAIVE APPROACH
 function checkIfTwoStringsAreAnagrams(str1  , str2){
 
