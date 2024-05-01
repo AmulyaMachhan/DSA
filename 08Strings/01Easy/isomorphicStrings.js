0b9f7751bbb72a5a98a601863f35454053f43fe4
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-01 20:44:41 +0530

    DSA Everyday

---
+++ 08Strings/01Easy/isomorphicStrings.js
@@ -0,0 +1,33 @@
+function isIsomorphic(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false; // Strings must have the same length to be isomorphic
+    }
+    
+    const map1 = new Map(); // Map to store mappings from str1 to str2
+    const map2 = new Map(); // Map to store mappings from str2 to str1
+    
+    for (let i = 0; i < str1.length; i++) {
+        const char1 = str1[i];
+        const char2 = str2[i];
+        
+        // If the mapping from str1 to str2 is inconsistent
+        if (map1.has(char1) && map1.get(char1) !== char2) {
+            return false;
+        }
+        // If the mapping from str2 to str1 is inconsistent
+        if (map2.has(char2) && map2.get(char2) !== char1) {
+            return false;
+        }
+        
+        // Create mappings from str1 to str2 and from str2 to str1
+        map1.set(char1, char2);
+        map2.set(char2, char1);
+    }
+    
+    return true; // Strings are isomorphic
+}
+
+// Example usage
+console.log(isIsomorphic("egg", "add")); // Output: true
+console.log(isIsomorphic("foo", "bar")); // Output: false
+console.log(isIsomorphic("paper", "title")); // Output: true
