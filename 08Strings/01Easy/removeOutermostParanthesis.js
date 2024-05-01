a79abb77727a3b1d470e40e485d000103efbe221
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-01 19:09:16 +0530

    DSA Everyday

--- 08Strings/01Easy/removeOutermostParanthesis.js
+++ 08Strings/01Easy/removeOutermostParanthesis.js
@@ -20,4 +20,27 @@ console.log(removeOutermostParanthesis("(()())(())"));
 console.log(removeOutermostParanthesis("(()())(())(()(()))"));
 
 //BRUTE FORCE APPROACH
-fucn
+function removeOutermostParanthesis_1(string){
+    let openCount = 0;
+    let res = "";
+
+    for(let i = 0 ; i < string.length ; i++){
+        if(string[i] === "("){
+            if(openCount > 0){
+                res += string[i];
+            }else{
+                openCount++;
+            }
+        }else{
+            openCount--;
+            if(openCount > 0){
+                res += string[i];
+            }
+        }
+    }
+
+    return res;
+}
+
+console.log(removeOutermostParanthesis_1("(()())(())"));
+console.log(removeOutermostParanthesis_1("(()())(())(()(()))"));
