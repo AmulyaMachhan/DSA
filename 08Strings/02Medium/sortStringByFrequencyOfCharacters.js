ceaef39a88f74618d65cd795c8d1a8bb02235629
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-02 05:26:10 +0530

    DSA Everyday

--- 08Strings/02Medium/sortStringByFrequencyOfCharacters.js
+++ 08Strings/02Medium/sortStringByFrequencyOfCharacters.js
@@ -1,3 +1,9 @@
+//PROBLEM STATEMENT
+//Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+// Return the sorted string. If there are multiple answers, return any of them.
+
+// BRUTE FORCE APPROACH
 function sortStringByFrequencyOfCharacters(string){
     let frq = {};
     for(let i = 0 ; i < string.length ; i++){
