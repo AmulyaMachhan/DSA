6319349b813c9a137a33f3ba8f4871236c98af9f
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-04 00:55:21 +0530

    Sum of squares of a number

--- 07Binary search/02 BS On Answers/sumOfSquares.js
+++ 07Binary search/02 BS On Answers/sumOfSquares.js
@@ -1,4 +1,20 @@
-function sumofSquaresOfANumber(number){
+// PROBLEM STATEMENT
+// Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+// Example 1:
+// Input: c = 5
+// Output: true
+// Explanation: 1 * 1 + 2 * 2 = 5
+
+// Example 2:
+// Input: c = 3
+// Output: false
+ 
+// Constraints:
+// 0 <= c <= 231 - 1
+
+// OPTIMAL APPROACH
+function sumofSquaresOfANumber_1(number){
     let low = 0, high = number;
 
     while(low <= high){
@@ -12,7 +28,7 @@ function sumofSquaresOfANumber(number){
     return false;
 }
 
-console.log(sumofSquaresOfANumber(5));
-console.log(sumofSquaresOfANumber(3));
-console.log(sumofSquaresOfANumber(5));
-console.log(sumofSquaresOfANumber(1000000000));
+console.log(sumofSquaresOfANumber_1(5));
+console.log(sumofSquaresOfANumber_1(3));
+console.log(sumofSquaresOfANumber_1(5));
+console.log(sumofSquaresOfANumber_1(1000000000));
