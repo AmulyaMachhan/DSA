88d58a3523fc0fdcdf6f035c974916cb4e5b152b
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-05 02:11:27 +0530

    Relative sort

--- 06Arrays/01Easy/relativeSortArrays.js
+++ 06Arrays/01Easy/relativeSortArrays.js
@@ -4,6 +4,38 @@
 //Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. 
 //Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
 
+//OPTIMAL SOLUTION 
+//Using counting sort
+function relativeSortArrays_2(arr1 , arr2){
+    let max = Math.max(...arr2);
+
+    const countArray = new Array(max + 1).fill(0);
+
+    for(let i = 0; i < arr1.length; i++){
+        countArray[arr1[i]]++;
+    }
+
+    const result = [];
+    for(let i = 0; i < arr2.length; i++){
+        while(countArray[arr2[i]] === 0){
+            result.push(arr2[i]);
+            countArray[arr2[i]]--;
+        }
+    }
+
+    for(let i = 0; i <= max; i++){
+        while(countArray[arr1[i]] > 0){
+            result.push(arr1[i]);
+            countArray[arr1[i]]--;
+        }
+    }
+
+    return result;
+}
+
+console.log(relativeSortArrays_2([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]));
+console.log(relativeSortArrays_2([28,6,22,8,44,17], [22,28,8,6]));
+
 //BETTER SOLUTION
 //USING HASHMAP FOR COUNTING AND SORTING
 function relativeSortArrays_1(arr1 , arr2){
