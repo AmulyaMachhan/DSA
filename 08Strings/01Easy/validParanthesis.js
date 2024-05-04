02b1fbe2fc2af09601a16590b8d9976f6b63e51a
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-05 02:00:00 +0530

    peakelementin2dmatrix

--- 08Strings/01Easy/validParanthesis.js
+++ 08Strings/01Easy/validParanthesis.js
@@ -6,3 +6,26 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
+
+function isValidParanthesis(string){
+    let stack = [];
+
+    for(let i = 0 ; i < string.length ; i++){
+        if(string[i] == "(" || string[i] == "{" || string[i] == "[") {
+            stack.push(string[i]);
+        }
+        else{
+            if(!stack.length ||
+            string[i] == ")" && stack[stack.length - 1] !== "(" ||
+            string[i] == "}" && stack[stack.length - 1] !== "{" ||
+            string[i] == "]" && stack[stack.length - 1] !== "[" ) {
+                return false
+            }
+            stack.pop();
+        }
+    }
+    return !stack.length;
+}
+
+console.log(isValidParanthesis("(){}[]"));
+console.log(isValidParanthesis("(]"));
