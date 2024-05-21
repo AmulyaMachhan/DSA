ca8e5300d5db530e7731e236796eefef91b61854
AmulyaMachhan <machhanamulya@gmail.com>
2024-05-22 00:01:34 +0530

    first occurence of a string

--- 08Strings/01Easy/firstOccurenceOfString.js
+++ 08Strings/01Easy/firstOccurenceOfString.js
@@ -1,3 +1,23 @@
+//OPTIMAL APPROACH
+//SLIDING WINDOW AND TWO POINTERS
+function firstOccurenceOfString_1(haystack , needle){
+    for(let i = 0 ; i < haystack.length - needle.length ; i++){
+        let found = true;
+        for(let j = 0; j < needle.length ; j++){
+            if(haystack[i + j] !== needle[j]){
+                found = false;
+                break;
+            }
+        }
+        if(found)
+            return i;
+    }
+    return -1;
+}
+
+console.log(firstOccurenceOfString_1("sadbutsad" , "sad"));
+console.log(firstOccurenceOfString_1("leetcode" , "leeto"));
+
 //BRUTE FORCE APPROACH
 function firstOccurenceOfString( haystack , needle){
     for(let i = 0 ; i < haystack.length ; i++){
