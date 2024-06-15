b64362497820782393b8b504ac31100e0cba1f12
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-15 14:02:05 +0530

    Seggregate odd and even indices in a linked list

--- 09Linked Lists/03MediumLLProblems/seggregateOddAndEvenIndicesInLL.js
+++ 09Linked Lists/03MediumLLProblems/seggregateOddAndEvenIndicesInLL.js
@@ -60,3 +60,24 @@ function seggregateOddAndEvenIndicesInLL(head){
 }
 
 seggregateOddAndEvenIndicesInLL(head);
+
+// APPROACH 2
+function seggregateOddAndEvenIndicesInLL_1(head){
+    let odd = head;
+    let even = head.next;
+
+    let evenHead = even;
+
+    while(even && even.next){
+        odd.next = odd.next.next;
+        even.next = even.next.next;
+        odd = odd.next;
+        even = even.next;
+    }
+
+    odd.next = evenHead;
+
+    return odd;
+}
+
+seggregateOddAndEvenIndicesInLL_1(head);
