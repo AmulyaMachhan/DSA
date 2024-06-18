38c85f7356dc1378520372d3f92dd8d11a130344
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-18 17:40:02 +0530

    Intersection of Two Linked Lists

--- 09Linked Lists/03MediumLLProblems/intersectionOfLL.js
+++ 09Linked Lists/03MediumLLProblems/intersectionOfLL.js
@@ -76,6 +76,48 @@ head2.next.next.next = head1.next.next.next
 // 3. Move the dummy pointer of the larger list by the difference achieved. This makes our search length reduced to a smaller list length.
 // 4. Move both pointers, each pointing two lists, ahead simultaneously if both do not collide.
 
+function getDifference(head1, head2){
+    let len1 = 0, len2 = 0;
+    while(head1 !== null || head2 !== null){
+        if(head1 !== null){
+            len1++;
+            head1 = head1.next;
+        }
+        else if(head2 !== null){
+            len2++;
+            head2 = head2.next
+        }
+    }
+    return len1 - len2;
+}
+
+function intersectionOfLinkedLists_2(head1 , head2){
+    let diff = getDifference(head1 , head2);
+    if(diff > 0){
+        while(diff !== 0){
+            head1 = head1.next;
+            diff--;
+        }
+    }else{
+        while(diff !== 0){
+            head2 = head2.next;
+            diff++;
+        }
+    }
+
+    while(head1 !== null){
+        if(head1 == head2){
+            return head1;
+        }
+        head1 = head1.next;
+        head2 = head2.next;
+    }
+
+    return 0;
+}
+
+console.log(intersectionOfLinkedLists_2(head1 , head2));
+
 // BETTER APPROACH 
 // Using hashing 
 function intersectionOfLinkedLists_1(head1 , head2){
