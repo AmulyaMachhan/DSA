edc47f918ffe20efaaf588b44d9514f080b3b4fb
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-20 18:33:45 +0530

    Sort 0s 1s 2s in a Linked List

--- 09Linked Lists/03MediumLLProblems/sort0s1s2sInLL.js
+++ 09Linked Lists/03MediumLLProblems/sort0s1s2sInLL.js
@@ -1,3 +1,31 @@
+// PROBLEM STATEMENT
+// Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
+
+// Example 1:
+// Input:
+// N = 8
+// value[] = {1,2,2,1,2,0,2,2}
+// Output: 0 1 1 2 2 2 2 2
+// Explanation: All the 0s are segregated
+// to the left end of the linked list,
+// 2s to the right end of the list, and
+// 1s in between.
+
+// Example 2:
+// Input:
+// N = 4
+// value[] = {2,2,0,1}
+// Output: 0 1 2 2
+// Explanation: After arranging all the
+// 0s,1s and 2s in the given format,
+// the output will be 0 1 2 2.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(N).
+
+// Constraints:
+// 1 <= N <= 106
+
 class Node {
     constructor(val, next) {
         this.val = (val === undefined ? 0 : val);
