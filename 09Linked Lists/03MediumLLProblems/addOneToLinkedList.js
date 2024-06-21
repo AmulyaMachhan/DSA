c18840ce988284ab99c8c09cb7a4d1d93bba5bd1
AmulyaMachhan <machhanamulya@gmail.com>
2024-06-21 15:03:22 +0530

    Add 1 to a Linked List

--- 09Linked Lists/03MediumLLProblems/addOneToLinkedList.js
+++ 09Linked Lists/03MediumLLProblems/addOneToLinkedList.js
@@ -1,3 +1,24 @@
+// PROBLEM STATEMENT
+// A number n is represented in Linked List such that each digit corresponds to a node in linked list. 
+// You need to add 1 to it. 
+// Returns the head of the modified linked list. The driver code prints the number.
+// Note: The head represents the left-most digit of the number.
+
+// Examples :
+// Input: LinkedList: 4->5->6
+// Output: 457
+// Explanation: 4->5->6 represents 456 and when 1 is added it becomes 457. 
+
+// Input: LinkedList: 1->2->3
+// Output: 124 
+// Explanation:  1->2->3 represents 123 and when 1 is added it becomes 124. 
+
+// Expected Time Complexity: O(n).
+// Expected Auxiliary Space: O(1).
+
+// Constraints:
+// 1 <= n <= 1021
+
 function addOneToLinkedList(head){
     const reverse = (head) => {
         let temp = head;
@@ -26,7 +47,7 @@ function addOneToLinkedList(head){
         if (carry === 0) {
             break;
         }
-        
+
         if(temp.next == null && carry > 0){
             temp.next = new Node(carry);
             carry = 0;
